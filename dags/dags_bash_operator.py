73aa9449d99dfaacd5260a11d8701686ef2d5c7a
pedroyoung95 <jty9531@gmail.com>
2024-05-28 21:00:20 +0900

    bash_operator dag 업로드

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -15,7 +15,7 @@ with DAG(
         bash_command="echo whoami",
     )
      
-    bash_t2 = BashOperator(
+     bash_t2 = BashOperator(
         task_id="bash_t2",
         bash_command="echo $HOSTNAME",
     )
