e288495f2041e1a9f2d161f2afb0cbec59b1f462
pedroyoung95 <jty9531@gmail.com>
2024-07-10 21:49:50 +0900

    dag template 2차 수정 재업로드

--- dags/dags_bash_with_templates.py
+++ dags/dags_bash_with_templates.py
@@ -4,7 +4,7 @@ import pendulum
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_select_fruit",
+    dag_id="dags_bash_with_templates",
     schedule="10 0 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
