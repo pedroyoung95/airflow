0a468c83ad05d0bbfee5fe9ebbe147ee0954e92f
pedroyoung95 <jty9531@gmail.com>
2024-08-19 20:25:09 +0900

    python&bash xcom 활용 수정

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -4,7 +4,7 @@ from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_with_xcom",
+    dag_id="dags_bash_python_with_xcom",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2024, 4, 1, tz="Asia/Seoul"),
     catchup=False
