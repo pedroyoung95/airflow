ad208404ec090d4f59a26a46d3fda6dfeb7e1f2c
pedroyoung95 <jty9531@gmail.com>
2025-07-17 17:51:18 +0900

    airflow 3.0반영

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -24,25 +24,25 @@ with DAG(
         python_callable=select_random
     )
 
-    def common_func(**kwargs) :
+    def common_func(**kwargs):
         print(kwargs['selected'])
 
     task_a = PythonOperator(
-        task_id = 'task_a',
-        python_callable = common_func,
-        op_kwargs = {'selected : A'}
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected':'A'}
     )
 
     task_b = PythonOperator(
-        task_id = 'task_b',
-        python_callable = common_func,
-        op_kwargs = {'selected : B'}
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected':'B'}
     )
 
     task_c = PythonOperator(
-        task_id = 'task_c',
-        python_callable = common_func,
-        op_kwargs = {'selected : C'}
-    )    
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected':'C'}
+    ) 
 
     python_branch_task >> [task_a, task_b, task_c]
