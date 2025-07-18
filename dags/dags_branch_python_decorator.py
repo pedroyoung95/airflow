91d36c8cee8ab2836608a98c79a2d37b9d27f15e
pedroyoung95 <jty9531@gmail.com>
2025-07-18 09:00:05 +0900

    branch decorator

--- dags/dags_branch_python_decorator.py
+++ dags/dags_branch_python_decorator.py
@@ -4,7 +4,7 @@ from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import DAG
 
 with DAG(
-    dag_id = "dags_branch_python_operator",
+    dag_id = "python_branch_decorator",
     start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
     schedule='0 1 * * *',
     catchup = False
