1eb119ba4a453a8d5ad1c27d33aa338a94480209
pedroyoung95 <jty9531@gmail.com>
2025-07-20 21:08:50 +0900

    http operator

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -4,7 +4,7 @@ from airflow.sdk import DAG, task
 
 with DAG(
     dag_id='dags_simple_http_operator',
-    start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2025, 7, 1, tz='Asia/Seoul'),
     catchup=False,
     schedule=None
 ) as dag:
@@ -12,7 +12,7 @@ with DAG(
     tb_new_books_info = HttpOperator(
         task_id = 'tb_new_books_info',
         http_conn_id = 'openapi.seoul.go.kr',
-        endpoint = '{{var.value.apikeyapikey_openapi_seoul_go_kr}}/json/SeoulLibNewArrivalInfo/1/100/',
+        endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/SeoulLibNewArrivalInfo/1/10/',
         method = 'GET',
         headers = {'Content-Type' : 'application/json',
                    'charset' : 'utf-8',
