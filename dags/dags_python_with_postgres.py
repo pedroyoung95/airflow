1f6d9d5d044d1a3edada46848443973de037a8bf
pedroyoung95 <jty9531@gmail.com>
2025-07-27 15:13:53 +0900

    insert postgres

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -17,10 +17,10 @@ with DAG(
                 task_id = kwargs.get('ti').task_id
                 run_id = kwargs.get('ti').run_id
                 msg = 'insert 수행'
-                sql = 'insert into py_opr_dict_insrt values (%s, %s, %s, %s);'
+                sql = 'insert into py_opr_drct_insrt values (%s, %s, %s, %s);'
                 cursor.execute(sql, (dag_id, task_id, run_id, msg))
                 conn.commit()
-                
+
     insrt_postgres = PythonOperator(
         task_id = 'insrt_postgres',
         python_callable = insrt_postgres,
